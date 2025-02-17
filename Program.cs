d6e641e50f0c6e01c19d187dde1b8045ac351b81
ADMIN <ADMIN@01-LAB01>
2025-02-17 13:45:34 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        int intA, intB;
+        double dbekq;
+        Console.WriteLine(" --------------- CHUONG TRINH GIAI PHUONG TRINH BAC 1 --------------- ");
+        Console.WriteLine(" ------------------------------- aX + b = 0 ------------------------- ");
+        Console.WriteLine("Nhap a: ");
+        intA = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Nhap b: ");
+        intB = Convert.ToInt32(Console.ReadLine());
+        if (intA == 0 && intB == 0)
+        {
+            Console.WriteLine(" Phuong trinh co vo so nghiem");
+        }
+        else if (intA == 0 && intB != 0)
+        {
+            Console.WriteLine(" Phuong trinh vo nghiem");
+        }
+        else
+        {
+            dbekq = (double)-intB / intA;
+            Console.WriteLine(" phuong trinh co 1 nghiem duy nhat x = {0}", dbekq);
+        }
+    }
+}
